f02ef41551d497bd56ecb0ee427916f9dd96b11c
lequoc2877 <21521357@gm.uit.edu.vn>
2022-11-25 17:11:58 +0700

    add setup files for setup board

---
+++ setup_board/setup_board.sh
@@ -0,0 +1,8 @@
+sudo apt install python3-pip
+sudo apt install python3-pil
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install adafruit_pca9685
+pip3 install adafruit-circuitpython-servokit
+pip3 install luma.oled
+pip3 install pycuda
