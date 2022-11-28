1877e26b5274225f81c9464c05dbe27a926e6683
Kino <vantin2912@gmail.com>
2022-11-28 09:21:59 +0700

    Remove Delay Test Code

--- Harware Control/TestCar.py
+++ Harware Control/TestCar.py
@@ -13,7 +13,6 @@ def BTN3_Func(chann):
 
 Car.regBTN(1,BTN1_Func)
 Car.setMotorMode(0)
-time.sleep(0.001)
 Car.setSpeed_cm(90)
 
 CurrentPos = Car.getPosition_cm()
@@ -23,7 +22,6 @@ while(Car.getPosition_cm() - CurrentPos < 1000):
     time.sleep(0.1)
     print("Car Pos ", Car.getPosition_cm())
 Car.setSpeed_cm(0)
-time.sleep(1)
 print("End Pos ", Car.getPosition_cm())
 
 print("Motor Mode 0 Done")
@@ -33,7 +31,7 @@ print("Start Motor Mode 1")
 Car.setMotorMode(1)
 Car.setAngle(-30)
 Car.setPosition_cm(1000, 70)
-time.sleep(0.5)
+# time.sleep(0.5)
 while(Car.getSpeed_cm()!= 0):
     time.sleep(0.1)
     print("Car Speed ", Car.getSpeed_cm())
